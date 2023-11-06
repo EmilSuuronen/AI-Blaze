c2076078d8c08279390b52233d9602d99a8edba5
Sofvi <laitinen.suvi99@gmail.com>
2023-11-06 13:53:45 +0200

    styling

--- src/Login/LoginView.js
+++ src/Login/LoginView.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "./Login.css";
 
 function LoginView() {
-    return (
-        <div>
-            <h2>Login page</h2>
-            <p>This is login page</p>
-        </div>
-    );
+  return (
+    <div>
+      <div className="container">
+        <h1 className="heading">AI-Blaze</h1>
+        <input type="text" className="input" placeholder="Username" />
+        <input type="password" className="input" placeholder="Password" />
+        <button className="button">Login</button>
+      </div>
+      <hr className="hr" />
+      <p className="or">OR</p>
+      <div className="container2">
+        <button className="google">Need an account? SIGN UP</button>
+      </div>
+    </div>
+  );
 }
 
 export default LoginView;
