e26cab598876006de36e7585912f1311b621beee
Sofvi <laitinen.suvi99@gmail.com>
2023-11-06 14:24:22 +0200

    merge with loginUI

--- src/Login/LoginView.js
+++ src/Login/LoginView.js
@@ -5,19 +5,21 @@ import { Link } from "react-router-dom";
 function LoginView() {
   return (
     <div>
-      <Link to="/">
-        <button>Back</button>
-      </Link>
       <div className="container">
         <h1 className="heading">AI-Blaze</h1>
         <input type="text" className="input" placeholder="Username" />
         <input type="password" className="input" placeholder="Password" />
         <button className="button">Login</button>
       </div>
-      <hr className="hr" />
-      <p className="or">OR</p>
+      <div>
+        <hr className="hr" />
+        <p className="or">OR</p>
+      </div>
       <div className="container2">
         <button className="google">Need an account? SIGN UP</button>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
       </div>
     </div>
   );
