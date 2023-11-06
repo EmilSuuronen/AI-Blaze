d67058ba738b05783bc0379d2b89e930c0f48d71
Sofvi <laitinen.suvi99@gmail.com>
2023-11-06 12:55:09 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import './App.css';
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import LoginView from "./Login/LoginView";
 
 export default function App() {
-    return (
-        HomeScreen()
-    );
-};
-
-function HomeScreen() {
-    return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/loginview" element={<LoginView />} />
+      </Routes>
+    </Router>
+  );
 }
 
+function HomeScreen() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Home</h1>
+      <Link to="/loginview">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/Login/LoginView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginView() {
+    return (
+        <div>
+            <h2>Login page</h2>
+            <p>This is login page</p>
+        </div>
+    );
+}
+
+export default LoginView;
