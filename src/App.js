2143122940d96d770af684282ab249e12687c9ea
Sofvi <laitinen.suvi99@gmail.com>
2023-11-06 12:34:36 +0200

    removed react-native

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, Text } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Map from './Map';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="App">
-                <Stack.Screen name="App" component={HomeScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        HomeScreen()
     );
 };
 
 function HomeScreen() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            <h1>Home</h1>
+        </div>
     );
 }
 
