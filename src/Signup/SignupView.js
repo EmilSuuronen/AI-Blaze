4659035d768b201d8fad060b1c5e77d2e17cf831
Sofvi <laitinen.suvi99@gmail.com>
2023-11-15 17:50:33 +0200

    User conditional rendering

--- src/Signup/SignupView.js
+++ src/Signup/SignupView.js
@@ -12,37 +12,38 @@ import "../Login/Login.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
-import { auth } from '../firebaseConfig';
+import { auth } from "../firebaseConfig";
 
 const SignupView = () => {
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState(false)
-  const [passwordError, setPasswordError] = useState(false)
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleSignUp = (event) => {
     event.preventDefault();
 
-    if (email === '') {
-      setEmailError(true)
+    if (email === "") {
+      setEmailError(true);
     }
-    if (password === '') {
-      setPasswordError(true)
+    if (password === "") {
+      setPasswordError(true);
     }
 
     if (email && password) {
-      console.log(email, password)
+      console.log(email, password);
     }
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log(userCredential)
-      }).catch((error) => {
-        console.log(error)
+        console.log(userCredential);
       })
-}
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
@@ -52,73 +53,73 @@ const SignupView = () => {
     <div>
       <div className="container_s">
         <h1 className="heading">AI-Blaze</h1>
-        <form 
-          autoComplete="off" 
+        <form
+          autoComplete="off"
           onSubmit={handleSignUp}
           className="formContainer"
-          >
-          <TextField 
+        >
+          <TextField
             label="Email"
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             required
             variant="outlined"
             type="email"
-            sx={{mb: 3}}
+            sx={{ mb: 3 }}
             fullWidth
             value={email}
             error={emailError}
           />
           <FormControl fullWidth variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            className="passwordContainer"
-            type={showPassword ? 'text' : 'password'}
-            onChange={e => setPassword(e.target.value)}
-            fullWidth
-            error={passwordError}
-            label="Password"
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-           </FormControl>
-           <FormControl fullWidth variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Repeat Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            onChange={e => setPassword(e.target.value)}
-            fullWidth
-            error={passwordError}
-            label="Password"
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-           </FormControl>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              className="passwordContainer"
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              error={passwordError}
+              label="Password"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl fullWidth variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Repeat Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              error={passwordError}
+              label="Password"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
           <Button
             sx={{ margin: 5 }}
             size="large"
