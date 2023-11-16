d8ccddc09893c4f2034b229906cd7cd445692378
Sofvi <laitinen.suvi99@gmail.com>
2023-11-16 17:51:33 +0200

    Updated signup ui

--- src/Signup/SignupView.js
+++ src/Signup/SignupView.js
@@ -1,4 +1,4 @@
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import React, { useState } from "react";
 import {
   Button,
   FormControl,
@@ -7,15 +7,17 @@ import {
   InputLabel,
   OutlinedInput,
   TextField,
+  ThemeProvider,
 } from "@mui/material";
-import "../Login/Login.css";
-import React, { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
 import { auth } from "../firebaseConfig";
+import theme from "../theme"; 
+import "./Signup.css"; 
 
 const SignupView = () => {
-  const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -50,98 +52,92 @@ const SignupView = () => {
   };
 
   return (
-    <div>
-      <div className="container_s">
-        <h1 className="heading">AI-Blaze</h1>
-        <form
-          autoComplete="off"
-          onSubmit={handleSignUp}
-          className="formContainer"
-        >
-          <TextField
-            label="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            variant="outlined"
-            type="email"
-            sx={{ mb: 3 }}
-            fullWidth
-            value={email}
-            error={emailError}
-          />
-          <FormControl fullWidth variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              className="passwordContainer"
-              type={showPassword ? "text" : "password"}
-              onChange={(e) => setPassword(e.target.value)}
+    <ThemeProvider theme={theme}>
+      <div className="signup_base">
+        <div className="signup_container">
+          <h1 className="signup_heading">AI-Blaze</h1>
+          <form autoComplete="off" onSubmit={handleSignUp} className="signup_formContainer">
+            <TextField
+              label="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              variant="outlined"
+              type="email"
+              sx={{ mb: 3 }}
               fullWidth
-              error={passwordError}
-              label="Password"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
+              value={email}
+              error={emailError}
             />
-          </FormControl>
-          <FormControl fullWidth variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">
-              Repeat Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? "text" : "password"}
-              onChange={(e) => setPassword(e.target.value)}
-              fullWidth
-              error={passwordError}
-              label="Password"
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <Button
-            sx={{ margin: 5 }}
-            size="large"
-            variant="contained"
-            className="loginButton"
-            type="submit"
-          >
-            Sign Up
-          </Button>
-        </form>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                className="passwordContainer"
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{ mb: 3 }}
+                fullWidth
+                error={passwordError}
+                label="Password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Repeat Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth
+                error={passwordError}
+                label="Password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <Button
+              style={{
+                backgroundColor: "rgba(79,81,140,1)",
+                color: "#fffffa",
+                marginTop: "10%",
+              }}
+              size="large"
+              variant="contained"
+              className="signupButton"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </form>
+        </div>
+        <div className="signup_container2">
+          <Link to="/loginview">
+            <Button>Already have an account? Log In</Button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <hr></hr>
-      </div>
-      <div className="container2">
-        <Link to="/loginview">
-          <Button sx={{ marginTop: 10, marginBottom: 5 }}>
-            Already had the account? Log In
-          </Button>
-        </Link>
-      </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
